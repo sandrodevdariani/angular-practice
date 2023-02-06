bd0fa1831dc222bd1ef4153847272b3fd7cf8c79
ssss <sdevdariani9@gmail.com>
2023-02-06 14:27:23 +0400

    angular routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { BooksModule } from "./books/books.module";
 import { CartComponent } from './cart/cart.component';
+import { AppRoutineModule } from "./app-routing.module";
 
 @NgModule({
     declarations: [AppComponent, CartComponent, ],
-    imports: [BrowserModule, FormsModule, BooksModule],  
+    imports: [BrowserModule, FormsModule, BooksModule, AppRoutineModule],  
     bootstrap: [AppComponent],
 }) 
 
